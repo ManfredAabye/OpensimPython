84c3f5945c9342e005543987769aa39224682300
ManfredAabye <zainhofer@gmx.de>
2024-08-26 11:55:18 +0200

    PythonNet 3.x .NET 8.0

--- pymodloader.py
+++ pymodloader.py
@@ -1,69 +1,64 @@
-import clr
-clr.AddReference('OpenSim.Framework')
-clr.AddReference('OpenSim.Region.Framework')
-
 import sys
-sys.path.append('/usr/lib/python2.6/') #'ScriptEngines/Lib') # stdlib seems to live here
+import importlib
 
-from OpenSim.Region.Framework.Interfaces import IRegionModule
-#import textcreate, pictureframe, toggleboxcolor, oukautil
-import webframeserver
+# Die .NET-Assemblies werden direkt importiert, wenn sie verfügbar sind
+from OpenSim.Region.Framework.Interfaces import IRegionModule # type: ignore
 
 class PyReloader(IRegionModule):
     upgradable = False
-    regpymods = [webframeserver] #textcreate, pictureframe, toggleboxcolor, oukautil]
+    regpymods = []  # Keine externen Module mehr erforderlich
     reginstances = []
 
     def Initialise(self, scene, configsource):
         self.scene = scene
         self.config = configsource
         scene.AddCommand(self, "py-reload", "py-reload", "...", self.cmd_py_reload)
-        print self, 'initialised with', scene
+        print(self, 'initialised with', scene)
 
     def PostInitialise(self):
-        print self, 'post-initialise'
-    
-    def Close(self):
-        print self, 'close'
+        print(self, 'post-initialise')
 
-    def getname(self):
+    def Close(self):
+        print(self, 'close')
+
+    @property
+    def Name(self):
         return "MyRegionModule"
 
-    Name = property(getname)
-
-    def isshared(self):
+    @property
+    def IsSharedModule(self):
         return False
-
-    IsSharedModule = property(isshared)
 
     def cmd_py_reload(self, modname, args):
         try:
             self.reload(modname, args)
-        except Exception, e:
-            print 'err or'
+        except Exception as e:
+            print('error')
             import traceback
             traceback.print_exc()
             raise
 
     def reload(self, modname, args):
-        print 'closing modules'
+        print('closing modules')
         for ri in self.reginstances:
-            print "found", ri, "in reginstances"
+            print("found", ri, "in reginstances")
             if ri.Name in self.scene.Modules:
-                print "also found in modules, so marking removed"
+                print("also found in modules, so marking removed")
                 ri.removed = True
-                print "removing", ri.Name, "from self.scene.Modules"
+                print("removing", ri.Name, "from self.scene.Modules")
                 self.scene.Modules.Remove(ri.Name)
             else:
-                print "not found in modules so not removing"
+                print("not found in modules so not removing")
             ri.Close()
 
         self.reginstances[:] = []
 
-        print 'reloading modules & looking for region classes'
+        print('reloading modules & looking for region classes')
         regclasses = []
+        # Keine Module mehr zum Nachladen vorhanden
+        # Dies ist nur ein Platzhalter, um zu zeigen, wie man Module lädt
         for m in self.regpymods:
-            reload(m)
+            importlib.reload(m)
             for name in dir(m):
                 o = getattr(m, name)
                 if name.startswith('_'):
@@ -74,18 +69,19 @@ class PyReloader(IRegionModule):
                     pass
                 else:
                     if x and getattr(o, 'autoload', None):
-                        print 'found', name
+                        print('found', name)
                         regclasses.append(o)
 
-        print 'instantiating found regions'
+        print('instantiating found regions')
         for klass in regclasses:
             ri = klass()
             ri.Initialise(self.scene, self.config)
             self.scene.AddModule(ri.Name, ri)
-            print "register instance", ri
+            print("register instance", ri)
             self.reginstances.append(ri)
             ri.PostInitialise()
-        print 'reload done'
+        print('reload done')
+
 
 loader = None
 
